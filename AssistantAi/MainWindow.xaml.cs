b70f042082614afcdbaac6a2ba719ddf7bf4b08b
Brad <bsoverns@gmail.com>
2025-02-27 23:58:56 -0500

    Added 4.5 preview

--- AssistantAi/MainWindow.xaml.cs
+++ AssistantAi/MainWindow.xaml.cs
@@ -106,7 +106,7 @@ namespace AssistantAi
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> gptModels = new List<string>() { "gpt-3.5-turbo", "gpt-4", "gpt-4-32k", "gpt-4o", "gpt-4o-mini", "o1-preview", "o1-mini" }; //These seem broken in the program, "gpt-4-32k" };
+        List<string> gptModels = new List<string>() { "gpt-3.5-turbo", "gpt-4", "gpt-4-32k", "gpt-4o", "gpt-4o-mini", "gpt-4.5-preview", "o1-preview", "o1-mini" }; //These seem broken in the program, "gpt-4-32k" };
         //List<string> gptModels = new List<string>() { "gpt-3.5-turbo", "gpt-4", "gpt-4-32k", "gpt-4o", "gpt-4o-mini" }; //o1 models are not available yet.
         List<string> whisperEndPoints = new List<string>() { "transcriptions", "translations" };
         List<string> ttsModels = new List<string>() { "tts-1", "tts-1-hd" }; //future use
