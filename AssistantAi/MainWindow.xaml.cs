6d3e13fec34911adf7d44bc21b1e73a979c02e87
Brad <bsoverns@gmail.com>
2026-04-05 09:13:33 -0400

    removed the test

--- AssistantAi/MainWindow.xaml.cs
+++ AssistantAi/MainWindow.xaml.cs
@@ -35,7 +35,6 @@ namespace AssistantAi
     /* Notes on prices 02/10/2024
     https://openai.com/pricing#language-models
     "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k" 
-    test
 
     GPT-4 Turbo
     Model	Input	Output
